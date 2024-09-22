6bca1707293c8158d9a6cba2e1445960aadc3489
josemarcellio <josemarcellio01@gmail.com>
2024-09-22 22:07:43 +0800

    ⚠️ Lupa menghapus give exp pada saat testing Job Skills ⚠️

--- src/main/java/com/josemarcellio/joseplugin/job/command/JobsCommand.java
+++ src/main/java/com/josemarcellio/joseplugin/job/command/JobsCommand.java
@@ -2,7 +2,6 @@ package com.josemarcellio.joseplugin.job.command;
 
 import com.josemarcellio.joseplugin.JosePlugin;
 import com.josemarcellio.joseplugin.job.gui.JobsGUI;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -20,38 +19,10 @@ public class JobsCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         if (commandSender instanceof Player player) {
-            if (strings.length < 4) {
-                JobsGUI jobsGUI = new JobsGUI(plugin);
-                jobsGUI.openGUI(player);
-                return true;
-            }
+            JobsGUI jobsGUI = new JobsGUI(plugin);
+            jobsGUI.openGUI(player);
+            return true;
         }
-
-        String action = strings[1];
-        String playerName = strings[2];
-        int amount;
-
-        try {
-            amount = Integer.parseInt(strings[3]);
-        } catch (NumberFormatException e) {
-            commandSender.sendMessage("Amount must be a number.");
-            return false;
-        }
-
-        Player targetPlayer = Bukkit.getPlayer(playerName);
-
-        if (targetPlayer == null) {
-            commandSender.sendMessage("Player not found.");
-            return false;
-        }
-
-        if (action.equalsIgnoreCase("give")) {
-            plugin.getJobsManager().giveExp(targetPlayer.getUniqueId(), amount);
-            commandSender.sendMessage("berhasil kirim " + amount + " experience ke " + playerName);
-        } else {
-            commandSender.sendMessage("/jobs exp give <nama> <jumlah>");
-        }
-
-        return true;
+        return false;
     }
 }
